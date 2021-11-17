4fc5cfe694033c88af73004986fcc666a5ed3cdf
Camella Enyi <enyicamella@gmail.com>
2021-11-17 14:03:30 -0800

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,6 @@
--- list all shows with their genre names
-SELECT tv_shows.title, tv_genres.name FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-ORDER BY tv_genres.name
+-- Script that lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+	FROM tv_shows
+	INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+	GROUP BY  tv_shows.title
+	ORDER BY rating	DESC;
