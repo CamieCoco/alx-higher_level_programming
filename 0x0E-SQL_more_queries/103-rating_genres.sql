389d83a92585950525f48eaccf92008ee8f95921
Camella Enyi <enyicamella@gmail.com>
2021-11-17 14:01:38 -0800

    Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,8 @@
--- show show titles by genre using join
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-WHERE ALL tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- Script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate)
+AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
