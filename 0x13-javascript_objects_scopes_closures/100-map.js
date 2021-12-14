d18b71e6753ab6e4a532d12b8b7d79609d2beb84
CamieCoco <75216613+CamieCoco@users.noreply.github.com>
2021-12-14 21:47:41 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-// script that imports an array and computes a new array
 const list = require('./100-data').list;
+const map1 = list.map((x, i) => i * x);
 console.log(list);
-let i = 0;
-const map1 = list.map(x => x * i++);
 console.log(map1);
